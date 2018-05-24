9dd0b0a0178a54ad978ef935f6f9d96a03ebd548
Gaetan Longree <gaetanlongree@gmail.com>
2018-05-24 14:29:39 +0200

    Update configuration via env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,21 @@
 
 # test is conf file is already present, if not copy the default one
 if [ ! -f /etc/powerdns/pdns.conf ]; then
-    cp -R /tmp/powerdns/* /etc/powerdns/
+cp -R /tmp/powerdns/* /etc/powerdns/
+
+sed -i '/launch\=/c\' /etc/powerdns/pdns.conf
+sed -i -e 's/^/\#/' /etc/powerdns/pdns.d/pdns.local.gpgsql.conf
+
+cat <<EOF >> /etc/powerdns/pdns.conf
+recursor=$DNS_FORWARDER
+launch=gpgsql
+gpgsql-host=$PGSQL_HOST
+gpgsql-user=$PGSQL_USER
+gpgsql-dbname=$PGSQL_DBNAME
+gpgsql-password=$PGSQL_PASSWD
+EOF
 fi
 
 # launch the PowerDNS service
 pdns_server
+
